4d0893587422e55af1977a21a750aaf1425ce40e
timmipetit <timmipetit@gmail.com>
2015-02-26 15:54:12 +0100

    Create a flask project

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Build the docker container with:
 
 Run it with:
 
-`docker run -e "ICALURL=https://some.url/calendar.ics" --rm recras/voipgridicaldestination`
+`docker run -p 5000:5000 -e "ICALURL=https://some.url/calendar.ics" --rm recras/voipgridicaldestination`
 
-The environment variable ICALURL should be a publicly accesible iCal-file that has full-day events
+This makes the website available at http://localhost:5000
+
+The environment variable ICALURL should be a publicly accesible iCal-file that has full-day events containing an '@' in the summary. The part before the '@' will be used as the value.
+
+http://localhost:5000 gives an overview of the dates & values
+http://localhost:5000/webhook gives an VoIPGRID compatible webhook output with a value for the current day.
+
--- main.py
+++ main.py
@@ -1,22 +1,43 @@
+import collections
 from icalendar import Calendar, Event
 from datetime import datetime
 from datetime import date
-import requests
 from os import getenv
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
 
 lookup = {}
-
 icalurl = getenv('ICALURL')
 if not icalurl:
     print 'export ICALURL env-var'
     quit()
 
-g = requests.get(str(icalurl))
+def refresh():
+    g = requests.get(str(icalurl))
 
-gcal = Calendar.from_ical(g.text)
-for component in gcal.walk():
-    if component.name == "VEVENT" and '@' in component.get('summary'):
-        lookup[component.get('dtstart').dt] = component.get('summary').__str__()
+    gcal = Calendar.from_ical(g.text)
+    for component in gcal.walk():
+        summary = component.get('summary').__str__()
+        if component.name == "VEVENT" and '@' in summary:
+            date = component.get('dtstart').dt
+            if  date >= date.today():
+                atIndex = summary.find('@')
+                lookup[date] = summary[:atIndex]
+
+@app.route('/')
+def main():
+    refresh()
+    return render_template('overview.html', lookup=collections.OrderedDict(sorted(lookup.items())))
 
 
-print 'Today: ' + lookup[date.today()]
+@app.route('/webhook/')
+def webhook():
+    return 'status=ACK&value=' + lookup[date.today()]
+
+
+if __name__ == "__main__":
+    refresh()
+    app.run(host= '0.0.0.0')
