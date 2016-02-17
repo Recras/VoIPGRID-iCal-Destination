ec385ed02b1d2f9432eb0d8218c858755c861382
Tijmen Klein <tijmen@recras.nl>
2016-02-17 12:25:16 +0100

    Use getenv

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ def webhook():
 
 if __name__ == "__main__":
     refresh()
-    port = int(os.environ.get('PORT',5000))
+    port = int(getenv('PORT',5000))
     app.run(host= '0.0.0.0', port=port)
