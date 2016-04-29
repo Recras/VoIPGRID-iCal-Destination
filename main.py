21793ea0064b2d3dc7e16722af2d8751a7fb4894
Tijmen Klein <tijmen@recras.nl>
2016-04-29 11:48:02 +0200

    Timer-based refresh are giving problems on Heroku

--- main.py
+++ main.py
@@ -3,33 +3,8 @@ from icalendar import Calendar, Event
 from datetime import datetime
 from datetime import date
 from os import getenv
-from threading import Timer, Thread
-from time import sleep
 from flask import Flask, render_template, request
 import requests
-
-class Scheduler(object):
-    def __init__(self, sleep_time, function):
-        self.sleep_time = sleep_time
-        self.function = function
-        self._t = None
-
-    def start(self):
-        if self._t is None:
-            self._t = Timer(self.sleep_time, self._run)
-            self._t.start()
-        else:
-            raise Exception("this timer is already running")
-
-    def _run(self):
-        self.function()
-        self._t = Timer(self.sleep_time, self._run)
-        self._t.start()
-
-    def stop(self):
-        if self._t is not None:
-            self._t.cancel()
-            self._t = None
 
 app = Flask(__name__)
 
@@ -69,13 +44,10 @@ def main():
 
 @app.route('/webhook/')
 def webhook():
+    refresh()
     return 'status=ACK&value=' + getToday()
 
 
 if __name__ == "__main__":
-    refresh()
-    scheduler = Scheduler(int(getenv('POLL_TIME',60)), refresh)
-    scheduler.start()
     port = int(getenv('PORT',5000))
     app.run(host= '0.0.0.0', port=port)
-    scheduler.stop()
